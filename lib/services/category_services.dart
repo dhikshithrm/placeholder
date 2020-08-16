61ca46b674366c6d0f5ded23482cbb616399274f
dhikshith12 <dhikshith12@users.noreply.github.com>
2020-08-16 15:58:22 +0530

    reverted

--- lib/services/category_services.dart
+++ lib/services/category_services.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Category_services{
-  List<Map<String,dynamic>> getCategories(){
-    List<Map<String, dynamic>> categories = [];
-     Firestore.instance
+  Future<List<Map<String,dynamic>>> getCategories()async{
+    List<Map<String,dynamic>> categories = [];
+     await Firestore.instance
            .collection('categories')
            .getDocuments().then((value){
              value.documents.forEach((e) {
